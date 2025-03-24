e06dd48e28cf95213418ff4c93ada7fa15881a6c
Matteo <matteonegridev@gmail.com>
2025-03-24 16:51:55 +0100

    created schema

--- src/mongoose/schema/schema.js
+++ src/mongoose/schema/schema.js
@@ -1,2 +1,26 @@
-import mongoose from "mongoose";
-import { Schema } from "mongoose";
+import { name } from "ejs";
+import mongoose, { Schema } from "mongoose";
+
+const inventorySchema = new mongoose.Schema({
+  name: {
+    type: mongoose.Schema.Types.String,
+    required: true,
+    trim: true,
+  },
+  category: {
+    type: mongoose.Schema.Types.String,
+  },
+  quantity: {
+    type: mongoose.Schema.Types.Number,
+  },
+  price: {
+    type: mongoose.Schema.Types.String,
+  },
+  expiryDate: {
+    type: mongoose.Schema.Types.String,
+  },
+});
+
+const Inventory = new Model("Inventory", inventorySchema);
+
+export default Inventory;
